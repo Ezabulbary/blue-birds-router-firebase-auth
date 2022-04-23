2cc1421d6c96ebe55e331dc56b71274308fdb9e6
Ezabulbary <ezabul.bary@gmail.com>
2022-04-23 19:31:40 +0600

    blue bird router

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Article from './component/Article/Article';
+import Header from './component/Header/Header';
+import Shop from './component/Shop/Shop';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Shop></Shop>
+      <Article></Article>
+    </div>
+  );
+}
+
+export default App;

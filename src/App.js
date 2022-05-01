cc3e6df68b5961a0ff741080960294181afad31c
Ezabulbary <ezabul.bary@gmail.com>
2022-05-01 11:07:28 +0600

    firebase authentication added email and password

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './component/About/About';
 import NotFound from './component/NotFound/NotFound';
 import Orders from './component/Orders/Orders';
 import Login from './component/Login/Login';
+import SignUp from './component/SignUp/SignUp';
 
 function App() {
   return (
@@ -13,11 +14,13 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/:shop' element={<Shop></Shop>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/shop' element={<Shop></Shop>}></Route>
         <Route path='/order' element={<Orders></Orders>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
--- src/component/SignUp/SignUp.js
+++ src/component/SignUp/SignUp.js
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
-import auth from '../../firebase.init';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init'
 
-const Login = () => {
+const SignUp = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
     const [error, setError] = useState('');
     const navigate = useNavigate();
 
-    const [signInWithEmailAndPassword, user,] = useSignInWithEmailAndPassword(auth);
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth);
 
-    if (user) {
-        navigate('/shop')
+    if(user){
+        navigate('/home')
     }
 
     const handleEmailBlur = event => {
@@ -26,34 +27,46 @@ const Login = () => {
 
     const handlePasswordBlur = event => {
         setPassword(event.target.value);
-        if (/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/.test(event.target.value)) {
+        if (/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/.test(event.target.value)){
             return (true)
         }
         setError("6 to 20 characters which contain at least one numeric digit, one uppercase and one lowercase letter!")
         return;
     }
 
-    const handleSignIn = event => {
-        event.preventDefault();
+    const handleConfirmPasswordBlur = event => {
+        setConfirmPassword(event.target.value);
     }
+
+    const handleSignUp = event => {
+        event.preventDefault();
+        if (password !== confirmPassword) {
+            setError("Password didn't match")
+            return;
+        }
+    }
+
 
     return (
         <div className='border-solid border-2 border-black rounded-lg bg-gray-100 m-8 md:m-auto md:w-96 md:my-20 p-10'>
-            <h2 className='text-3xl text-center m-10'>Please Sign In</h2>
-            <form onSubmit={handleSignIn}>
+            <h2 className='text-3xl text-center m-10'>Please Sign Up</h2>
+            <form onSubmit={handleSignUp}>
                 <p className='font-bold text-xl m-2'>Enter Email</p>
                 <input onBlur={handleEmailBlur} className='rounded p-2 w-full' type="email" placeholder='Type your email' />
 
                 <p className='font-bold text-xl m-2'>Enter Password</p>
                 <input onBlur={handlePasswordBlur} className='rounded p-2 w-full' type="password" placeholder='Type your password' />
 
+                <p className='font-bold text-xl m-2'>Confirm Password</p>
+                <input onBlur={handleConfirmPasswordBlur} className='rounded p-2 w-full' type="password" placeholder='Confirm your password' />
+
                 <p className='text-red-500'>{error}</p>
 
-                <button onClick={() => signInWithEmailAndPassword(email, password)} className='w-40 bg-orange-400 hover:text-white m-4 px-6 py-2 rounded block mx-auto'>Log In</button>
-                <small><Link className='text-blue-500' to='/signup'>Don't Have an Account?</Link></small>
+                <button onClick={() => createUserWithEmailAndPassword(email, password)} className='w-40 bg-orange-400 hover:text-white m-4 px-6 py-2 rounded block mx-auto'>Sign Up</button>
+                <small><Link className='text-blue-500' to='/login'>Already Have an Account?</Link></small>
             </form>
         </div>
     );
 };
 
-export default Login;
+export default SignUp;
